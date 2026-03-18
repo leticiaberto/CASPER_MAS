b08df7b4ea0f655ee2d261fa4e1b4aeda41cc963
leticia.berto <leticia.mberto@gmail.com>
2026-03-18 13:27:45 +0000

    Remove some variables to avoid freezing the screen

--- launch_container.sh
+++ launch_container.sh
@@ -17,24 +17,34 @@ CHECKSUM_FILE=".dockerfile_checksum"
 # ---------------------------
 # Allow Docker (root) to access X server
 # ---------------------------
-xhost +local:root
+xhost +SI:localuser:$(whoami)
+#xhost +local:docker #root
 
 
 # ---------------------------
-# Check Dockerfile changes and rebuild image if needed
+# Check Dockerfile / requirements / ros2_packages changes and rebuild image if needed
 # ---------------------------
-CURRENT_HASH=$(sha256sum $DOCKERFILE_PATH $REQUIREMENTS_PATH | sha256sum | awk '{print $1}')
+ROS2_DIR="ros2_packages"
+
+CURRENT_HASH=$(
+{
+    sha256sum "$DOCKERFILE_PATH"
+    sha256sum "$REQUIREMENTS_PATH"
+    find "$ROS2_DIR" -type f ! -path "*/build/*" ! -path "*/install/*" ! -path "*/log/*"
+} | sha256sum | awk '{print $1}'
+)
+
 OLD_HASH=""
 if [[ -f $CHECKSUM_FILE ]]; then
     OLD_HASH=$(cat $CHECKSUM_FILE)
 fi
 
 if [[ "$CURRENT_HASH" != "$OLD_HASH" ]] || [[ "$(docker images -q $IMAGE_NAME 2> /dev/null)" == "" ]]; then
-    echo "🔨 Dockerfile or requirements.txt changed, or image missing. Building $IMAGE_NAME..."
+    echo "🔨 Dockerfile, requirements.txt, or ros2_packages changed, or image missing. Building $IMAGE_NAME..."
     docker build -t $IMAGE_NAME -f $DOCKERFILE_PATH .
     echo "$CURRENT_HASH" > $CHECKSUM_FILE
 else
-    echo "✅ Dockerfile and requirements.txt unchanged, image exists. Skipping build."
+    echo "✅ No relevant changes detected. Skipping build."
 fi
 
 # ---------------------------
@@ -48,11 +58,12 @@ fi
 
 # ---------------------------
 # Enable NVIDIA GPU offload (in Docker or native)
+# Removed because it was freezing the screen and causing performance issues. It may be worth revisiting in the future if we can find a way to mitigate those issues.
 # ---------------------------
-export __NV_PRIME_RENDER_OFFLOAD=1
-export __GLX_VENDOR_LIBRARY_NAME=nvidia
-export __VK_LAYER_NV_optimus=NVIDIA_only
-export __NV_PRIME_RENDER_OFFLOAD_PROVIDER=NVIDIA-G0
+#export __NV_PRIME_RENDER_OFFLOAD=1
+#export __GLX_VENDOR_LIBRARY_NAME=nvidia
+#export __VK_LAYER_NV_optimus=NVIDIA_only
+#export __NV_PRIME_RENDER_OFFLOAD_PROVIDER=NVIDIA-G0
 
 # ---------------------------
 # Optional for better GL performance
@@ -63,19 +74,21 @@ export LIBGL_ALWAYS_INDIRECT=0
 # Launch ADIRL container (add --rm to auto-deletes on exit):
 # ---------------------------
 docker run -it \
-  --gpus all \
-  --env DISPLAY=$DISPLAY \
-  --env XDG_RUNTIME_DIR=$XDG_RUNTIME_DIR \
-  --env __NV_PRIME_RENDER_OFFLOAD=1 \
-  --env __GLX_VENDOR_LIBRARY_NAME=nvidia \
-  --env __VK_LAYER_NV_optimus=NVIDIA_only \
-  --volume /tmp/.X11-unix:/tmp/.X11-unix \
-  --env QT_X11_NO_MITSHM=1 \
-  --mount type=bind,source=/home/e10738lb/Development/CASPER_MAS,target=/app/CASPER_MAS,readonly=false,bind-propagation=rslave \
-  -v $(pwd)/ros2_packages:/ros2_ws/src \
-  -w /app/CASPER_MAS \
-  --name $CONTAINER_NAME \
-  $IMAGE_NAME 
+    --gpus all \
+    --device=/dev/dri \
+    -e NVIDIA_DRIVER_CAPABILITIES=all \
+    -e DISPLAY=$DISPLAY \
+    -e XDG_RUNTIME_DIR=$XDG_RUNTIME_DIR \
+    -e QT_X11_NO_MITSHM=1 \
+    -v /tmp/.X11-unix:/tmp/.X11-unix \
+    --ipc=host \
+    --mount type=bind,source=/home/e10738lb/Development/CASPER_MAS,target=/app/CASPER_MAS,readonly=false,bind-propagation=rslave \
+    -v $(pwd)/ros2_packages:/ros2_ws/src \
+    -w /app/CASPER_MAS \
+    --name $CONTAINER_NAME \
+    $IMAGE_NAME
+
+docker stop $CONTAINER_NAME
 
 # Revoke X access for security after exit
-xhost -local:root
+xhost -SI:localuser:$(whoami)

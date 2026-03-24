e50144b19193e53a8dfcd12aa6565deedf13f85f
leticia.berto <leticia.mberto@gmail.com>
2026-03-24 13:52:40 +0000

    Reorganize files

--- ros2_packages/robots_adapters/setup.py
+++ ros2_packages/robots_adapters/setup.py
@@ -12,7 +12,10 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        (os.path.join('share', package_name, 'launch'), glob('launch/*'))
+        (os.path.join('share', package_name, 'launch'), glob('launch/*')),
+        (os.path.join('share', package_name, 'urdf'), glob('urdf/*')),
+        (os.path.join('share', package_name, 'config'), glob('config/*')),
+        (os.path.join('share', package_name, 'scripts'), glob('scripts/*')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
--- ros2_packages/simulation/setup.py
+++ ros2_packages/simulation/setup.py
@@ -1,8 +1,18 @@
 from glob import glob
+import os
 
 from setuptools import find_packages, setup
 
 package_name = 'simulation'
+
+def get_files_recursive(directory):
+    paths = []
+    for (dirpath, dirnames, filenames) in os.walk(directory):
+        for filename in filenames:
+            filepath = os.path.join(dirpath, filename)
+            install_dir = os.path.join('share', package_name, dirpath)
+            paths.append((install_dir, [filepath]))
+    return paths
 
 setup(
     name=package_name,
@@ -11,6 +21,7 @@ setup(
     data_files=[
         ('share/' + package_name + '/launch', glob('launch/*.py')),
         ('share/' + package_name + '/worlds', glob('worlds/*.sdf')),
+        *get_files_recursive('models'),
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
@@ -19,7 +30,7 @@ setup(
     zip_safe=True,
     maintainer='Leticia Berto',
     maintainer_email='leticia.berto@manchester.ac.uk',
-    description='Simulation package for Franka Emika Panda robot',
+    description='Simulation package for Gazebo',
     license='Apache-2.0',
     entry_points={
         'console_scripts': [

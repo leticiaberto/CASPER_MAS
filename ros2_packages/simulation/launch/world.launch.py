48369aa1ce1abb5141cead4ffaed569fccce05c7
leticia.berto <leticia.mberto@gmail.com>
2026-04-28 18:13:20 +0100

    add human_adapters in the gazebo_resource

--- ros2_packages/simulation/launch/world.launch.py
+++ ros2_packages/simulation/launch/world.launch.py
@@ -58,6 +58,8 @@ def generate_launch_description():
                 os.path.dirname(get_package_share_directory('pmb2_description')),
                 os.path.dirname(get_package_share_directory('pal_gripper_description')),
                 os.path.dirname(get_package_share_directory('pal_urdf_utils')),
+                os.path.join(get_package_share_directory('human_adapters'), 'models'), 
+                os.environ.get('IGN_GAZEBO_RESOURCE_PATH', ''),
             ])
         ),
 
